8afa6d4a3b9fe45911b705921672bf64370e01d0
wil.i.am <wiliam.tae@gmail.com>
2019-07-22 21:14:42 -0300

    separated components

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,25 @@
+"use strict";
 import React from "react";
+import Search from "./Search";
+import UserInfo from "./UserInfo";
+import Actions from "./Actions";
+import Repos from "./Repos";
 
 const App = () => (
   <div className="app">
-    <div className="search">
-      <input type="search" placeholder="Digite o nome do usuário" />
-    </div>
-    <div className="user-info">
-      <img src="https://avatars0.githubusercontent.com/u/41973973?v=4" />
-      <h1>
-        <a href="https://github.com/wiliamtaesunglee">Wiliam Tae Sung Lee</a>
-      </h1>
-      <ul className="repos-info">
-        <li>- Repositórios: 20</li>
-        <li>- Seguidores: 10</li>
-        <li>- Seguindo: 10</li>
-      </ul>
-
-      <div className="actions">
-        <button>Ver repositórios</button>
-        <button>Ver favoritos</button>
-      </div>
-
-      <div className="repos">
-        <h2>Repositórios:</h2>
-        <ul>
-          <li>
-            <a href="#">Nome do repositório</a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="starred">
-        <h2>Favoritos:</h2>
-        <ul>
-          <li>
-            <a href="#">Nome do repositório</a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <Search />
+    <UserInfo />
+    <Actions />
+    <Repos
+      className="repos"
+      title="Repositórios:"
+      repos={[{ name: "Nome do Repositório", link: "#" }]}
+    />
+    <Repos
+      className="Starred"
+      title="Favoritos"
+      repos={[{ name: "Nome do Repositório", link: "#" }]}
+    />
   </div>
 );
 
---
+++ src/components/UserInfo.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import React from "react";
+
+const UserInfo = () => (
+  <div className="user-info">
+    <img src="https://avatars0.githubusercontent.com/u/41973973?v=4" />
+    <h1>
+      <a href="https://github.com/wiliamtaesunglee">Wiliam Tae Sung Lee</a>
+    </h1>
+    <ul className="repos-info">
+      <li>- Repositórios: 20</li>
+      <li>- Seguidores: 10</li>
+      <li>- Seguindo: 10</li>
+    </ul>
+  </div>
+);
+
+export default UserInfo;
