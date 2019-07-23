0c9447c2530a1f85085827e7ff2eba14390a2ae9
wil.i.am <wiliam.tae@gmail.com>
2019-07-23 08:41:55 -0300

    component state start to added

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,30 @@
 "use strict";
-import React from "react";
+import React, { Component } from "react";
 import Search from "./Search";
 import UserInfo from "./UserInfo";
 import Actions from "./Actions";
 import Repos from "./Repos";
 
-const App = () => (
-  <div className="app">
-    <Search />
-    <UserInfo />
-    <Actions />
-    <Repos
-      className="repos"
-      title="Repositórios:"
-      repos={[{ name: "Nome do Repositório", link: "#" }]}
-    />
-    <Repos
-      className="Starred"
-      title="Favoritos"
-      repos={[{ name: "Nome do Repositório", link: "#" }]}
-    />
-  </div>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Search />
+        <UserInfo />
+        <Actions />
+        <Repos
+          className="repos"
+          title="Repositórios:"
+          repos={[{ name: "Nome do Repositório", link: "#" }]}
+        />
+        <Repos
+          className="Starred"
+          title="Favoritos"
+          repos={[{ name: "Nome do Repositório", link: "#" }]}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
